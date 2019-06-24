e6ee6b2aafdc83c70a713080dac889d91d435283
JayJayTing <tingjayj@gmail.com>
2019-06-24 20:06:54 +0000

    done

--- Lighthouse-Weeks/w1/lotide/assertEqual.js
+++ Lighthouse-Weeks/w1/lotide/assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("These two arguemnts are the same!");
+    console.log(`These two arguemnts are the same: ${actual} vs ${expected}`);
   } else {
-    console.log("These two arguments are NOT the same!");
+    console.log(`These two arguemnts are NOT the same: ${actual} vs ${expected}`);
   }
     
 };
