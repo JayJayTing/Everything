b8796cee431318df81967294581ca43fb95b90a5
JayJayTing <tingjayj@gmail.com>
2019-06-26 00:15:09 +0000

    done

--- Lighthouse-Weeks/w1/lotide/eqObjects.js
+++ Lighthouse-Weeks/w1/lotide/eqObjects.js
@@ -1,54 +1,45 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`These two arguemnts are the same: ${actual} vs ${expected}`);
-    } else {
-      console.log(`These two arguemnts are NOT the same: ${actual} vs ${expected}`);
-    }
+  if (actual === expected) {
+    console.log(`These two arguemnts are the same: ${actual} vs ${expected}`);
+  } else {
+    console.log(`These two arguemnts are NOT the same: ${actual} vs ${expected}`);
+  }
       
-  };
+};
 
-  const eqArrays = function(array1, array2)
-  {
-    if(array1.length === array2.length)
-    {
-      for (var i = 0; i < array1.length; i ++)
-      {
-        if(array1[i] !== array2[i])
-        {
-          return false;
-        }
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i ++) {
+      if (array1[i] !== array2[i]) {
+        return false;
       }
+    }
   
-    }
-    else
-    {
-      return false;
-    }
+  } else {
+    return false;
+  }
   
   return true;
   
   
-  }
+};
 
 const eqObjects = function(object1, object2) {
-    if(Object.keys(object1).length === Object.keys(object2).length)
-    {
-        for(var key in object1)
-        {
-            if(Array.isArray(object1[key]) && Array.isArray(object2[key])){
-                return eqArrays(object1[key], object2[key]);
-            } else {
-                if(object1[key] !== object2[key])
-                {   
-                    return false;
-                }
-            }
-            
+  if (Object.keys(object1).length === Object.keys(object2).length) {
+    for (let key in object1) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        return eqArrays(object1[key], object2[key]);
+      } else {
+        if (object1[key] !== object2[key]) {
+          return false;
         }
-        return true;
-    }   else{
-        return false;
+      }
+            
     }
+    return true;
+  }   else {
+    return false;
+  }
 
 };
 
